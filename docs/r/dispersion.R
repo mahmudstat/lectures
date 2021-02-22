ae123f2f1979ae172a7507e3cdedae70d33f9cb7
mahmudstat <almahmud.sbi@gmail.com>
2021-02-22 12:28:29 +0600

    Add first fortn question

--- docs/r/dispersion.R
+++ docs/r/dispersion.R
@@ -21,3 +21,22 @@ cv <- function(x) sd(x)/mean(x)*100
 cqd <- function(x) {
   (quantile(x)[4]-quantile(x)[2])/(quantile(x)[4]+quantile(x)[2])*100
 }
+
+## Coefficient of Variation
+
+cv1 <- function(x) paste0(sd(x)/mean(x)*100, "%")
+
+cv2 <- function(mean, sd) paste0(round(sd/mean*100,2), "%")
+
+# Combined Mean and variance
+
+cmv <- function(n1, n2, m1, m2, v1, v2){
+  mc <- (n1*m1+n2*m2)/(n1+n2)
+  d1 <- m1-mc
+  d2 <- m2-mc
+  comv <- (n1*(v1^2+d1^2)+n2*(v2^2+d2^2))/(n1+n2)
+  comsd <- sqrt(comv)
+  list("Combined Mean" = mc, "Combined Variance" = comv,
+       "Combined SD" = comsd)
+}
+
